9f2d813c2a5a1699615eac0da73c7d20c968f188
hhhhhq <Mr.j@tom.com>
2021-03-11 21:30:33 +0800

    before develop home page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,18 @@
 <template>
   <div>
-    <span class="msg">
-      hello world
-    </span>
-    <van-button @click="jump" type="primary">按钮</van-button>
+
   </div>
 </template>
 
 <script>
+import { post } from '@/utils/request'
+
 export default {
   methods: {
-    jump() {
-      this.$router.replace('/pages/index/main')
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .msg {
-    color: red;
-    font-size: 20px;
-    font-weight: bold;
-  }
+
 </style>
